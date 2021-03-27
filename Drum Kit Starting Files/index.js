244450e5e312c168c501df77519efe904154d148
pavantanniru <63087702+pavantanniru@users.noreply.github.com>
2021-03-27 11:44:09 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,97 @@
+
+
+
+var leng = document.querySelectorAll(".drum").length;
+
+
+
+
+
+
+for (var i = 0; i < leng ; i++) {
+
+
+
+
+  document.querySelectorAll(".drum")[i].addEventListener('click',function(){
+
+  // console.log(this.innerHTML);
+  var button_key = this.innerHTML;
+  makeSound(button_key);
+
+  addAnimation(button_key);
+
+  });
+
+
+}
+
+
+
+document.addEventListener("keydown",function(event){
+  makeSound(event.key);
+  addAnimation(event.key);
+});
+
+
+function makeSound(key) {
+
+
+
+switch (key) {
+  case "w":
+
+  var tom1 = new Audio('sounds/tom-1.mp3');
+  tom1.play();
+
+
+    break;
+
+
+
+  case "a":
+    var tom2 = new Audio('sounds/tom-2.mp3');
+    tom2.play();
+  break;
+
+  case "s":
+  var tom3 = new Audio('sounds/tom-3.mp3');
+  tom3.play();
+  break;
+
+  case "d":
+  var tom4 = new Audio('sounds/tom-4.mp3');
+  tom4.play();
+  break;
+
+   case "j":
+   var snare = new Audio('sounds/snare.mp3');
+   snare.play();
+   break;
+
+   case "k":
+   var crash = new Audio('sounds/crash.mp3');
+   crash.play();
+   break;
+
+    case "l":
+    var kick = new Audio('sounds/kick-bass.mp3');
+    kick.play();
+    break;
+
+  default:
+  console.log(this.innerHTML);
+
+}
+}
+
+
+function addAnimation(btn){
+
+    document.querySelector("." + btn).classList.add("pressed");
+
+    setTimeout( function () {
+      document.querySelector('.'+btn).classList.remove('pressed');
+    },100 );
+
+}
